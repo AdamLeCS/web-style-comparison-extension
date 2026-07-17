a072994ecc5cad0d21556f6f516ce2d7002a69ad
Adam Le <adamle1025@gmail.com>
2026-07-17 13:40:35 -0400

    able to send files using fs

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import http from 'node:http';
+import fs from 'node:fs';
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    res.end("Hi");
-    
+    fs.readFile('./test.html', (err, data) => {
+        if (err) {
+            console.log("Error occured: ", err);
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
 });
 
 server.listen(3000);
