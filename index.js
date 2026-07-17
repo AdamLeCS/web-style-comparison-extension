8b812edcddfbebb77d72da593b3ac305e2d04505
Adam Le <adamle1025@gmail.com>
2026-07-17 12:44:12 -0400

    local host server opening

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import http from 'node:http';
 
-const server = http.createServer()
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    res.end("Hi");
+    
+});
+
+server.listen(3000);
